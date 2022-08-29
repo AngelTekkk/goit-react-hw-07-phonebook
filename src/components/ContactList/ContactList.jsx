65c951c2744df79f8b46560f332d3a357f7fcf59
Andriy Makarov <angeltek69@gmail.com>
2022-08-29 15:28:55 +0200

    Fullfield

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { useDeleteContactMutation } from '../../redux';
-import { Notify } from 'notiflix';
 import ContactListItem from 'components/ContactListItem/ContactListItem';
 import s from './ContactList.module.css';
 
 export default function ContactList({ visibleContacts }) {
-  const [deleteContact] = useDeleteContactMutation();
-
   return (
     <ul className={s.list}>
       {visibleContacts &&
